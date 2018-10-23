a62a7c1b8ea6ecd2f4b115aac2d351e8f9a95460
Scott <scot@udel.edu>
2018-10-23 18:52:51 -0400

    survey started

--- movieAPI/src/app/app.module.ts
+++ movieAPI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { SurveyComponent } from './survey/survey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule,
