76b90864598ce7d41cdbb4d6222680c5b59f9dbf
Jurban36 <Jennymurban@gmail.com>
2018-10-24 18:30:38 -0400

    working getting data from survey

--- movieAPI/src/app/survey/survey.component.ts
+++ movieAPI/src/app/survey/survey.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AppComponent } from '../app.component';
+import { Component, OnInit, Input, Injectable } from '@angular/core';
+
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class SurveyComponent implements OnInit {
-  genreString = "";
 
-  
+  title = 'movieAPI';
+  actorsMoviesTogether;
+  finalData;
+  desiredFormat = "Movie"
+  //Both
+  genreList="&with_genres=";
+  languageList="&language=";
+  maxRuntime = "&with_runtime.lte=120";
+  minRuntime = "&with_runtime.gte=60";
+  genre = "Comedy";
+  //Movie
+  actorIDs = "&with_people=";
+  actorName="Ben Stiller Owen Wilson";
+  goodMovies = "&vote_average.gte=6"
+  badMovies = "&vote_average.lte=6"
+  releaseDateBefore = "&primary_release_date.lte=2010-01-01"
+  releaseDateAfter = "&primary_release_date.gte=2001-01-01"
+  //TV
+  tvRating = "&vote_average.gte=8";
+  airedBefore = "&first_air_date.lte=2016-01-01";
+  airedAfter = "&first_air_date.gte=2010-01-01";
 
-  constructor(private util:AppComponent) { }
+  constructor() { }
   submitMovie() {
 
-    this.util.construct();
+    // this.util.construct();
     
-    setTimeout(() => {
-      console.log("hellooooo")
-      this.util.randomFunction();
-      console.log(this.util.actorName)
-    }, 2000);
+    // setTimeout(() => {
+    //   console.log("hellooooo")
+    //   this.util.randomFunction();
+    //   console.log(this.util.actorName)
+    // }, 2000);
     // this.util.randomFunction();
     // this.util.pullGenre();
     // this.util.pullLanguage();
