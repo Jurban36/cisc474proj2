718dd9b27fd6ae7474b58dc1e4a3678e9d4be55a
unknown <rbutler@udel.edu>
2018-10-24 21:30:35 -0400

    more front end

--- movieAPI/src/app/carousel/carousel.component.ts
+++ movieAPI/src/app/carousel/carousel.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-carousel', 
-    templateUrl: './carousel.component.html'
+    templateUrl: './carousel.component.html',
+    providers: [NgbCarouselConfig]
 })
 export class CarouselComponent {
   images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 4000;
+  }
 }
